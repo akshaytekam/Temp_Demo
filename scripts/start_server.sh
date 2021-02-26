41fb6601274972dbc19296b742fd4174fa5b857e
Akshay Tekam <tekamakshay11@gmail.com>
2021-02-26 16:19:29 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-#sudo systemctl start gunicorn
-#sudo systemctl status gunicorn
-#sudo service nginx restart	
+sudo systemctl start gunicorn
+sudo systemctl status gunicorn
+sudo service nginx restart	
