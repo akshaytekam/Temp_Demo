832eeb9a0ba80eb61c432de012871bbe71887f44
Akshay Tekam <tekamakshay11@gmail.com>
2021-02-27 10:13:23 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-source /home/ubuntu/env/bin/activate
-cd /home/ubuntu/new_chatapp-master
+source /home/ec2-user/env/bin/activate
+cd /home/ec2-user/new_chatapp
 pip3 install django bcrypt django-extensions
-pip3 install -r /home/ubuntu/new_chatapp-master/requirements.txt
+pip3 install -r /home/ec2user/new_chatapp/requirements.txt
 pip3 install gunicorn
  
